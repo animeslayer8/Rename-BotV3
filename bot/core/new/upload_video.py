deb029d14c097321a6b1d53f628322488f4c10a7
Katsuki <126319219+animeslayer8@users.noreply.github.com>
2023-04-13 16:34:03 +0530

    Update upload_video.py

--- bot/core/new/upload_video.py
+++ bot/core/new/upload_video.py
@@ -1,4 +1,4 @@
-# (c) @AbirHasan2005
+# (c) @Katsuki_warlord
 
 import os
 import time
@@ -113,7 +113,7 @@ class UploadVideo:
             thumbnail = thumb
         if not caption:
             caption = f"**File Name:** `{os.path.basename(video)}`" \
-                      "\n\n**@AH_RenameBot**"
+                      "\n\n**@Kuski_bot**"
         c_time = time.time()
         await self.send_video(
             chat_id=chat_id,
